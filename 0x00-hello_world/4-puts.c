19d7c5dbaeaa341f350fa98c3a894d66d600939f
yf01 <yf01@gmail.com>
2023-07-12 23:24:25 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+/**
+ * main - A program that prints the size of the various computer types
+ * Return 0 (success)
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
