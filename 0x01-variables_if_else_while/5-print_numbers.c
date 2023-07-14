6f9127ea4881e31da43112c1c9d541f12b02ab43
yf01 <yf01@gmail.com>
2023-07-14 16:49:07 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+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
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+for (i = 0; i < 10; i++)
+{
+printf("%d\n", i);
+}
+return (0);
+}
