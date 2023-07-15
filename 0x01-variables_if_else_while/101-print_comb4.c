fbc9a8d127612cbc85d9bc85bb0e954453cdbad9
yf01 <yf01@gmail.com>
2023-07-14 17:55:52 -0700

    101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i, j, k;
+for (i = 0; i < 10; i++)
+{
+for (j = i + 1; j < 10; j++)
+{
+for (k = 0; k < 10; k++)
+{
+putchar('0' + i);
+putchar('0' + j);
+putchar('0' + k);
+if (k < 7 || i < 8 || j < 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
