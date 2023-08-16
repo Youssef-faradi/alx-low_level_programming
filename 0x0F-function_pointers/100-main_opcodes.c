0b9339b56325104165682c69a9685ca3aef40b06
faradiyoussef <faradiyoussef1@gmail.com>
2023-08-16 11:29:44 +0100

    functionspointers

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -29,4 +29,15 @@ int main(int argc, char *argv[])
 
 	arr = (char *)main;
 
+	for (i = 0; i < bytes; i++)
+	{
+		if (i == bytes - 1)
+		{
+			printf("%02hhx\n", arr[i]);
+			break;
+		}
+		printf("%02hhx ", arr[i]);
+	}
+	return (0);
+}
 
